330765166e8035e66ff0c0451ccaab87ea57c618
julcsi <ejulcsi@gmail.com>
2018-11-23 17:27:34 +0100

    subscribe inp

--- subscribe.js
+++ subscribe.js
@@ -1,4 +1,5 @@
-const url = 'https://script.google.com/macros/s/AKfycbz7dH_CBshOK2O2ngctNHlDvkGioeaIH6-4aoIJJXag_H_KRc5h/exec'
+const macro = 'https://script.google.com/macros/s/AKfycbz7dH_CBshOK2O2ngctNHlDvkGioeaIH6-4aoIJJXag_H_KRc5h/exec'
+const form = document.getElementById('subscribe-form')
 
 const button = document.getElementById('subscribe-btn')
 
@@ -6,9 +7,11 @@ button.addEventListener('click', e => {
   e.preventDefault()
 
   const input = document.querySelector('#subscribe-form input')
+  const url = `${macro}?email=${encodeURIComponent(input.value)}`
 
-  window.fetch(`${url}?email=${input.value}`, {
+  window.fetch(url, {
     method: 'get',
-    mode: "cors",w // no-cors, cors, *same-origin
-  }).then(resp => console.log(resp)).catch(e => console.log(e))
+    mode: "cors", // no-cors, cors, *same-origin
+  }).then(resp => console.log(url, resp))
+    .catch(e => console.log('error', e))
 })
