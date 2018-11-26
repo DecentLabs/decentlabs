848ee1e4b211f59250905545e0332c21c9ca92a4
juli_e <julia.ero@euedge.com>
2018-11-26 13:07:58 +0100

    add subscribe form

--- subscribe.js
+++ subscribe.js
@@ -1,17 +1,7 @@
-const macro = 'https://script.google.com/macros/s/AKfycbz7dH_CBshOK2O2ngctNHlDvkGioeaIH6-4aoIJJXag_H_KRc5h/exec'
 const form = document.getElementById('subscribe-form')
+const success = document.getElementById('success-msg')
 
-const button = document.getElementById('subscribe-btn')
-
-button.addEventListener('click', e => {
-  e.preventDefault()
-
-  const input = document.querySelector('#subscribe-form input')
-  const url = `${macro}?email=${encodeURIComponent(input.value)}`
-
-  window.fetch(url, {
-    method: 'get',
-    mode: "cors", // no-cors, cors, *same-origin
-  }).then(resp => console.log(url, resp))
-    .catch(e => console.log('error', e))
+form.addEventListener('submit', e => {
+  success.style.display = 'block'
+  form.style.display = 'none'
 })
