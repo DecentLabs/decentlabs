ec9d867de266fddec638a08c12546194ee8877dc
juli_e <julia.ero@euedge.com>
2018-11-27 17:20:51 +0100

    upcoming with form

--- subscribe.js
+++ subscribe.js
@@ -1,7 +1,24 @@
 const form = document.getElementById('subscribe-form')
 const success = document.getElementById('success-msg')
 
-form.addEventListener('submit', e => {
-  success.style.display = 'block'
-  form.style.display = 'none'
+const button = document.getElementById('subscribe-btn')
+
+button.addEventListener('click', e => {
+  e.preventDefault()
+
+  const input = document.querySelector('#subscribe-form input')
+  const url = 'https://docs.google.com/forms/d/e/1FAIpQLSeYHOaZvdM6vMgpOaXsHBOtLjYVRq-fYuYnnz9XtwKqKKcrkQ/formResponse'
+  const data = {
+    email: input.value
+  }
+
+  window.fetch(url, {
+    method: 'post',
+    mode: "cors", // no-cors, cors, *same-origin,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(resp => console.log(url, resp))
+    .catch(e => console.log('error', e))
 })
