c823685a9041ec2aa342fef79b5d6211a4eb92a7
brigittaforrai <forraibrigi@gmail.com>
2019-02-19 18:42:40 +0100

    mobile styles

--- decent.js
+++ decent.js
@@ -2,15 +2,16 @@ const header = document.querySelector('header')
 const ratio = 2.585
 const colors = ['#e8a87c','#c38d9e','#8bcbc8','#40b3a3', '#4CC1E5']
 let width = window.innerWidth
+let height = 700
 // let height = width / ratio
-height = 700
+// let height = 700 = document.querySelector('header')
 let animations = []
 
 init()
 
 function init () {
   // header.style.setProperty('height', '700px')
-  header.style.setProperty('height', height + 'px')
+  // header.style.setProperty('height', height + 'px')
   templating ()
   resize()
   createBubbles()
@@ -69,7 +70,6 @@ function hover () {
     let node = item.children[index]
 
     item.addEventListener('touchstart', () => {
-      console.log('touch');
       if (node) {
         let color = colors[Math.round(Math.random() * colors.length-1)]
         node.style.backgroundColor = color + 'F2'
@@ -102,8 +102,7 @@ function hover () {
 function resize () {
   window.addEventListener('resize', () => {
     width = window.innerWidth
-    // height = width / ratio
-    header.style.setProperty('height', height + 'px')
+    // header.style.setProperty('height', height + 'px')
     animations.forEach((anim) => {
       anim.update()
     })
