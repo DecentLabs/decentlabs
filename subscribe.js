c3b0a838652590264414e318723af5e8f2a7f55f
brigittaforrai <forraibrigi@gmail.com>
2020-05-15 12:36:36 +0200

    save ip and location

--- subscribe.js
+++ subscribe.js
@@ -4,22 +4,30 @@ const email = document.querySelector('#subscribe-form input[name="email"]')
 button.addEventListener('click', e => {
   e.preventDefault()
   const macro = 'https://script.google.com/macros/s/AKfycbwDd_e0hXCszwt6mJaMuMHHsxndEkzKbkXmRCdHyHOaLnfwxdE/exec'
-  const url = `${macro}?email=${encodeURIComponent(email.value)}`
+  const getIp = 'https://ipapi.co/json/'
 
-  if (email.value) {
-    window.fetch(url, {
-      method: 'get',
-      mode: 'cors',
-    }).then(resp => {
-      console.log(resp.status)
-      const form = document.getElementById('subscribe-form')
-      const success = document.getElementById('success-msg')
-      success.style.display = 'block'
-      form.style.display = 'none'
-    }).catch(e => console.log('error', e))
-  } else {
-    email.classList.add('error')
-  }
+  window.fetch(getIp, {
+    method: 'get'
+  }).then(resp => {
+    resp.json().then(data => {
+      const url = `${macro}?email=${encodeURIComponent(email.value)}
+                           &ip=${encodeURIComponent(data.ip)}
+                           &location=${encodeURIComponent(data.city)}`
+      if (email.value) {
+        window.fetch(url, {
+          method: 'get',
+          mode: 'cors'
+        }).then(resp => {
+          const form = document.getElementById('subscribe-form')
+          const success = document.getElementById('success-msg')
+          success.style.display = 'block'
+          form.style.display = 'none'
+        }).catch(e => console.log('error', e))
+      } else {
+        email.classList.add('error')
+      }
+    })
+  })
 })
 
 email.addEventListener('keyup', () => {
